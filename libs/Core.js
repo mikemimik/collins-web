92f2e8a36b4c8757af4244d74d1b7fa29f4a54b8
afrolionx <mike@mikecorp.ca>
2016-04-28 21:47:28 -0400

    setup core.init() and switched express for http module

--- libs/Core.js
+++ libs/Core.js
@@ -11,16 +11,60 @@ const async = require('async');
 const _ = require('lodash');
 
 // INFO: npm-service-modules
-const Express = require('express');
+const Http = require('http');
 
 class CollinsWeb extends Emitter.EventEmitter {
-  constructor(config) {
+  constructor (config) {
     super();
     this.config = config;
+    this.logger = this.config.logger;
+    delete this.config.logger;
     this.initialized = false;
     this.cogs = [];
     this.actions = [];
-    this._client = null;
     this.Runtime = require('../utils/Runtime');
   }
+
+  init (next) {
+    async.series([
+      Loader.initConfig.bind(this),
+      Lodder.initGear.bing(this),
+      Loader.initCogs.bind(this),
+      Loader.initListeners.bind(this)
+    ], (err, results) => {
+      if (err) {
+
+        // INFO: there was an error processing the config
+        // TODO: emit an error up to collins
+        next(err);
+      }
+      results.forEach((result) => {
+        if (result) {
+          // INFO: loader returned error data
+        }
+      });
+
+      this.initialized = true;
+
+      // INFO: all the initializations have been complted
+      this.logger.gear('TESTING', this.constructor.name, 'finished init', 'RESULT:', results);
+      next(err);
+    });
+  }
+
+  connect (next) {
+
+    next(null);
+  }
+
+  disconnect (next) {
+
+    next(null);
+  }
+
+  log () {
+
+  }
 }
+
+module.exports = CollinsWeb;
--- utils/Loader.js
+++ utils/Loader.js
@@ -2,24 +2,44 @@
 
 // INFO: service-gear specific modules
 const Listeners = require('../libs/Listeners');
+const CoreError = require('../libs/CoreError');
 
 // INFO: common modules
 const async = require('async');
 const _ = require('lodash');
 
+// INFO: npm-service-module
+const Http = require('http');
+
 class Loader {
-  static initConfig(next) {
+  static initConfig (next) {
+    this.logger.gear('CollinsWeb', 'Loader', 'initConfig', 'this:', this); // TESTING
+
+    // TODO: validate config
     next(null);
   }
-  static initGear(next) {
+
+  static initGear (next) {
+
+    // TODO: pass config options to express()
+    this.Runtime['client'] = Http.createServer();
+
     next(null);
   }
-  static initCogs(next) {
+
+  static initCogs (next) {
+
+    // TODO: config cogs which will be middleware for express
     next(null);
   }
-  static initListeners(next) {
+
+  static initListeners (next) {
+    let listeners = Listeners.getMethods();
+    async.each(listeners, (listener, each_cb) => {
+
+    });
     next(null);
   }
 }
 
-module.exprots = Loader;
+module.exports = Loader;
