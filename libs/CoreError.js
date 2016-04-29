36f741f8b082a1452e502fd7480855c4399a9a28
afrolionx <mike@mikecorp.ca>
2016-04-28 21:46:37 -0400

    finished stubbing out error class

--- libs/CoreError.js
+++ libs/CoreError.js
@@ -1,10 +1,28 @@
 'use strict';
 
 class ExtendableError extends Error {
-  constructor(message) {
+  constructor (message) {
     super(message);
     this.name = this.constructor.name;
     this.message = message;
     Error.captureStackTrace(this, this.constructor.name);
   }
 }
+
+class WebError extends ExtendableError {
+  constructor(type, data) {
+    super('constructor'); // TODO: is this correct?
+    this.data = (data) ? data : {};
+    this.type = type;
+    this.message = '\'' + type + '\' error message received';
+    let reason = data.details || data.reason;
+    if (reason) {
+      this.message += ':\n"' + reason + '"\n';
+    } else {
+      this.message += '. ';
+    }
+    this.message += 'See \'data\' for details.';
+  }
+}
+
+module.exports = WebError;
